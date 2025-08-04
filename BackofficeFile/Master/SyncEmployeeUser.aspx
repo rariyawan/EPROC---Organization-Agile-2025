765c91166cb9c52a22c83f631d77756053a92e78
rariyawan <78006435+rariyawan@users.noreply.github.com>
2025-08-04 17:16:00 +0700

    Add files via upload

---
+++ BackofficeFile/Master/SyncEmployeeUser.aspx
@@ -0,0 +1,166 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SyncEmployeeUser.aspx.cs" Inherits="Master_SyncEmployeeUser" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+<link rel="stylesheet" type="text/css" href="../Styles/pips/Style_doctype.css"/>
+<script type="text/javascript" src="../js/calendarWidget.js"></script>
+<script language="javascript" type="text/javascript" src="../js/jquery-1.2.3.js"></script>
+<script language="javascript" type="text/javascript" src="../js/modal-window.js"></script>
+<script language="javascript" type="text/javascript" src="../js/utils.js"></script>
+<script language="javascript" type="text/javascript">
+    function clickInfo(strParam) {
+        openMyModal('../procurement/PRMonitoringInfo.aspx?' + strParam, 1000, 450, "auto");
+    }
+
+    function clickFlow(strParam) {
+        openMyModal('../procurement/FlowControlmonitoring_Fetch.aspx?' + strParam, 1000, 450, "auto");
+    }
+
+    function lov_organization() {
+        getFromLOV('', 'txtOrganizationName', 'LovOrganizationName.aspx');
+    }
+
+    function clickAll(obj) {
+        objdtl = document.getElementsByName("chkEmp");
+        for (d = 0; d < objdtl.length; d++) {
+            document.getElementsByName("chkEmp")[d].checked = obj.checked;
+        }
+    }
+
+    function clickSync() {
+        objdtl = document.getElementsByName("chkEmp");
+        var intSelect = 0;
+        for (d = 0; d < objdtl.length; d++) {
+            if (document.getElementsByName("chkEmp")[d].checked) {
+                intSelect = 1;
+                break;
+            }
+        }
+
+        if (intSelect == 0) {
+            alert("No Employee(s) selected");
+            return false;
+        } else {
+            if (confirm('Are you sure to sync. selected employee ?')) {
+                document.getElementById("btnSync").style.display = "none";
+                document.getElementById("textLoading").style.display = "";
+            }
+        }
+        
+    }
+</script>
+</head>
+<body style="MARGIN: 0px" marginheight="0" marginwidth="0">
+<form id="form1" runat="server">
+<table align="left" width="100%">
+  <tr>
+    <td>
+        <table cellspacing="0" cellpadding="0" width="100%" border="0">
+        <tr>
+          <td><img border="0" src="../images_tab/tab_s_on.gif"></td> 
+          <td background="../images_tab/tab_on.gif" nowrap align="center"><b>Sync. Employee User</b></td> 
+          <td><img border="0" src="../images_tab/tab_e_on.gif"></td> 
+          <td background="../images_tab/tab_back.gif" width="100%">&nbsp;</td> 
+          <td><img border="0" src="../images_tab/tab_end.gif"></td>
+        </tr>
+      </table>
+         <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td valign="top">
+              <asp:Panel id="pnlFilter" runat="server">
+              <table class="Record" cellspacing="0" cellpadding="0">
+                <tr class="Controls">
+                  <td class="th">Username&nbsp;&nbsp; </td> 
+                  <td><asp:TextBox ID="txtUserName" runat="server" Columns="15" 
+                          CssClass="ControlsIN"></asp:TextBox>
+                      &nbsp;</td> 
+                  <td class="th">NIK / Nama&nbsp;&nbsp; </td> 
+                  <td><asp:TextBox ID="txtSearch" runat="server" Columns="32" 
+                        CssClass="ControlsIN"></asp:TextBox>
+                    &nbsp;</td> 
+                  <td class="th">Organisasi&nbsp;&nbsp; </td> 
+                  <td>
+                     <asp:TextBox id="txtOrganizationName" CssClass="ControlsIN" maxlength="64" Columns="35" runat="server"/>
+                     <img onclick="lov_organization()" border="0" src="../images/view.gif">&nbsp;
+                  </td> 
+                </tr>
+                <tr class="Controls">
+                  <td colspan="6" style="text-align: right">&nbsp; 
+                    <asp:Button ID="btnFilter" runat="server" CssClass="Button" Text="Find" 
+                          onclick="btnFilter_Click" />
+                      &nbsp;&nbsp;</td>
+                </tr>
+              </table>
+              </asp:Panel>
+            </td>
+          </tr>
+        </table>
+        <br /><br />
+      <asp:Panel id="pnlGrid" runat="server">
+          <table class="Header" border="0" cellspacing="0" cellpadding="0" width="100%">
+            <tr>
+            <td class="HeaderLeft"><img border="0" alt="" src="../Styles/pips/Images/Spacer.gif"></td> 
+            <td class="th">
+                    Sync Employee
+            </td> 
+            <td class="HeaderRight"><img border="0" alt="" src="../Styles/pips/Images/Spacer.gif"></td>
+            </tr>
+        </table>
+        <asp:GridView ID="dgvList" runat="server" AutoGenerateColumns="false" 
+        CellPadding="5" CellSpacing="0" PageSize="20" AllowPaging="false" PagerSettings-Visible="false" AllowCustomPaging="false"
+        UseAccessibleHeader="true" Width="100%" OnRowDataBound="dgvList_RowDataBound">
+        <HeaderStyle CssClass="Caption" />
+        <RowStyle CssClass="Row"/>
+        <AlternatingRowStyle CssClass="AltRow"/>
+        <Columns>
+            <asp:BoundField DataField="UserId_PK" HeaderText="ID"></asp:BoundField>
+            <asp:TemplateField HeaderText="<input type='checkbox' name='chkAll' id='chkAll' onclick='javascript:clickAll(this)'>" HeaderStyle-Width="2%" HeaderStyle-HorizontalAlign="Center" ItemStyle-VerticalAlign="Top" ItemStyle-HorizontalAlign="Center">
+                <ItemTemplate>
+                    <asp:Label ID="lblChk" runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:BoundField DataField="UserName" HeaderText="Username"></asp:BoundField>
+            <asp:BoundField DataField="EmployeeNo" HeaderText="NIK"></asp:BoundField>
+            <asp:BoundField DataField="FullName" HeaderText="Nama"></asp:BoundField>
+            <asp:BoundField DataField="OrganizationName" HeaderText="Organisasi"></asp:BoundField>
+            <asp:BoundField DataField="JobPositionName" HeaderText="Jabatan"></asp:BoundField>
+            <asp:BoundField DataField="Leader" HeaderText="Atasan Langsung"></asp:BoundField>
+            <asp:BoundField DataField="SM" HeaderText="SM"></asp:BoundField>
+            <asp:BoundField DataField="GM" HeaderText="GM"></asp:BoundField>
+            <asp:BoundField DataField="Director" HeaderText="Direksi"></asp:BoundField>
+            
+        </Columns>
+        </asp:GridView>
+      <asp:Label ID="lblNoRow" runat="server"></asp:Label>
+      <br />
+      </asp:Panel>
+        <asp:Panel ID="pnlPaging" runat="server">
+        <table border="0" cellpadding="0" cellspacing="0" width="99%" align="center">
+        <tr>
+        <td>
+            <asp:ImageButton ID="imbFirst" runat="server" onclick="imbFirst_Click"/>
+            <asp:ImageButton ID="imbPrev" runat="server" onclick="imbPrev_Click"/>
+            &nbsp;
+            <asp:Label ID="lblPaging" runat="server"></asp:Label>
+            &nbsp;
+            <asp:ImageButton ID="imbNext" runat="server" onclick="imbNext_Click"/>
+            <asp:ImageButton ID="imbLast" runat="server" onclick="imbLast_Click"/>
+            &nbsp; <span id="PRFormHolder" runat="server">
+            <asp:Button ID="btnSync" runat="server" CssClass="Button" OnClick="btnSync_Click" OnClientClick="return clickSync()" Text="Sync. from HCMS" />
+            <a id="textLoading" name="textLoading" style="display:none">Please wait while loading ...</a>
+            </span>
+        </td>
+        </tr>
+        </table>
+        </asp:Panel>
+      
+    </td>
+  </tr>
+</table>
+    
+</form>
+</body>
+</html>
---
+++ BackofficeFile/Master/SyncEmployeeUser.aspx.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using Company.Apps.Utility;
+using Company.Apps.WsDataPIPS;
+using Company.Apps.WsWorkflowEngine;
+using System.Configuration;
+using System.Web.Script.Serialization;
+using System.IO;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using Company.Apps.WsEmail;
+using System.Net;
+using ExcelLibrary.BinaryFileFormat;
+using DevExpress.XtraRichEdit.Layout.Export;
+using DevExpress.XtraRichEdit.Fields;
+using DevExpress.Xpo.Logger;
+
+public partial class Master_SyncEmployeeUser : System.Web.UI.Page
+{
+
+    ClsUtility ClsUtil = new ClsUtility();
+    WsDataPIPS WsData = new WsDataPIPS();
+    WsWorkflowEngine WsWF = new WsWorkflowEngine();
+    WsEmail wsemail = new WsEmail();
+
+    string DELETE_CONFIRM_MSG = ClsUtility.MessageDeleteConfirm();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ClsUtil.CheckSession();
+        if (!IsPostBack)
+        {
+            /*
+            string strEncryptedString = Request.QueryString[ConfigurationManager.AppSettings["ParamEncrypt"].ToString()];
+            string strDecryptedString = Server.UrlDecode(ClsCryptoEngine.Decrypt(strEncryptedString.Replace(" ", "+")).Replace("&amp;", "&"));
+            Response.Write(strDecryptedString);
+            */
+            
+            //ViewData(1);
+            pnlGrid.Visible = false;
+            pnlPaging.Visible = false;
+            txtOrganizationName.Attributes.Add("readonly", "readonly");
+
+            ViewData(1);
+        }
+    }
+    
+
+    protected void ViewData(int intCurrPage)
+    {
+        pnlGrid.Visible = true;
+        pnlFilter.Visible = true;
+        pnlPaging.Visible = true;
+
+        DataSet dsData = null;
+        DataTable dtData0 = null;
+        DataTable dtData1 = null;
+
+        dsData = WsData.RunSQL("begin " +
+                            "declare " +
+                            "@rowamount numeric, " +
+                            "@pagecount numeric; " +
+                            "exec pips.stp_GetEmployeeUser_fetch " +
+                            "'" + txtUserName.Text.Trim() + "', " +
+                            "'" + txtSearch.Text.Trim() + "', " +
+                            "'" + txtOrganizationName.Text.Trim() + "', " +
+                            "'" + ClsAuthSession.USERNAME + "', " +
+                            "" + intCurrPage + ", " +
+                            "50, " +
+                            "@rowamount out, " +
+                            "@pagecount out; " +
+                            "end;");
+        dtData0 = dsData.Tables[0];
+        dtData1 = dsData.Tables[1];
+        dgvList.DataSource = dtData0;
+        dgvList.DataBind();
+
+        long intRowAmount = Convert.ToInt64(dtData1.Rows[0]["RowAmount"]);
+        long intPageCount = Convert.ToInt64(dtData1.Rows[0]["PageCount"]);
+        ViewState["pageCount"] = intPageCount;
+        ViewState["currPage"] = intCurrPage;
+
+        if (dtData0.Rows.Count == 0)
+        {
+            lblNoRow.Text = ClsUtility.MessageNoData();
+            dgvList.Visible = false;
+            lblNoRow.Visible = true;
+        }
+        else
+        {
+            dgvList.Visible = true;
+            lblNoRow.Text = "";
+            lblNoRow.Visible = false;
+        }
+
+        ClsUtil.LoadImagePaging(intCurrPage, 50, intPageCount, imbFirst, imbPrev, imbNext, imbLast, lblPaging);
+        
+    }
+
+
+    protected void dgvList_RowDataBound(Object sender, GridViewRowEventArgs e)
+    {
+
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            string strUserId = e.Row.Cells[0].Text;
+            string strEmpNo = e.Row.Cells[3].Text;
+            Label lblChk = (Label)e.Row.Cells[1].FindControl("lblChk");
+
+            lblChk.Text = "<input type='checkbox' value='" + strEmpNo + "' name='chkEmp' id='chkEmp'>";
+        }
+
+        e.Row.Cells[0].Visible = false;
+
+    }
+
+    protected void imbNext_Click(object sender, ImageClickEventArgs e)
+    {
+        int intCurrPage = Convert.ToInt32(ViewState["currPage"]) + 1;
+        ViewState["currPage"] = intCurrPage;
+        ViewData(intCurrPage);
+    }
+    protected void imbLast_Click(object sender, ImageClickEventArgs e)
+    {
+        int intCurrPage = Convert.ToInt32(ViewState["pageCount"]);
+        ViewState["currPage"] = intCurrPage;
+        ViewData(intCurrPage);
+    }
+    protected void imbPrev_Click(object sender, ImageClickEventArgs e)
+    {
+        int intCurrPage = Convert.ToInt32(ViewState["currPage"]) - 1;
+        ViewState["currPage"] = intCurrPage;
+        ViewData(intCurrPage);
+    }
+    protected void imbFirst_Click(object sender, ImageClickEventArgs e)
+    {
+        int intCurrPage = 1;
+        ViewState["currPage"] = intCurrPage;
+        ViewData(intCurrPage);
+    }
+
+    protected void btnFilter_Click(object sender, EventArgs e)
+    {
+        int intCurrPage = 1;
+        ViewState["currPage"] = intCurrPage;
+        ViewData(intCurrPage);
+    }
+
+
+    protected void btnSync_Click(object sender, EventArgs e)
+    {
+
+        try
+        {
+
+            string strFilter_username = txtUserName.Text.Trim();
+            string strFilter_search = txtSearch.Text.Trim();
+            string strFilter_organization = txtOrganizationName.Text.Trim();
+
+            DataSet dsEmp;
+            DataTable dtEmp;
+
+            DataSet dsLead;
+            DataTable dtLead;
+
+            string strEmpList = Request["chkEmp"];
+            string[] arrEmp = strEmpList.Split(',');
+            int i;
+            string strEmp = string.Empty;
+
+            string strUsername = string.Empty;
+            string strEmployeeNo = string.Empty;
+            string strEmployeeName = string.Empty;
+            string strMailAddress = string.Empty;
+            string strOrganizationCode = string.Empty;
+            string strOrganizationName = string.Empty;
+            string strJobPositionCode = string.Empty;
+            string strJobPositionName = string.Empty;
+
+            string strLeader = string.Empty;
+            string strSM = string.Empty;
+            string strGM = string.Empty;
+            string strDir = string.Empty;
+
+            for (i = 0; i < arrEmp.Length; i++)
+            {
+                strEmp = arrEmp[i].Trim();
+
+                dsEmp = WsData.RunSQL("begin exec pips.stp_APIToHCM_GetEmployee " +
+                                    "'" + strEmp + "', " +
+                                    "'" + ClsAuthSession.USERNAME + "'; end;");
+                dtEmp = dsEmp.Tables[0];
+
+                dsLead = WsData.RunSQL("begin exec pips.stp_APIToHCM_GetAtasanFull " +
+                                    "'" + strEmp + "', " +
+                                    "'" + ClsAuthSession.USERNAME + "'; end;");
+                dtLead = dsLead.Tables[0];
+
+                strUsername = dtEmp.Rows[0]["userName"].ToString();
+                strEmployeeNo = dtEmp.Rows[0]["employeeNo"].ToString();
+                strEmployeeName = dtEmp.Rows[0]["employeeName"].ToString();
+                strMailAddress = dtEmp.Rows[0]["mailAddress"].ToString();
+                strOrganizationCode = dtEmp.Rows[0]["organizationCode"].ToString();
+                strOrganizationName = dtEmp.Rows[0]["organizationName"].ToString();
+                strJobPositionCode = dtEmp.Rows[0]["jobPositionCode"].ToString();
+                strJobPositionName = dtEmp.Rows[0]["jobPositionName"].ToString();
+
+                strLeader = dtLead.Rows[0]["Leader_employeeNo"].ToString();
+                strSM = dtLead.Rows[0]["SM_employeeNo"].ToString();
+                strGM = dtLead.Rows[0]["GM_employeeNo"].ToString();
+                strDir = dtLead.Rows[0]["Director_employeeNo"].ToString();
+
+                string strExec = WsData.ExecuteScalar("begin " +
+                                                "declare " +
+                                                "@intOutStatus numeric, " +
+                                                "@strOutMessage varchar(max); " +
+                                                "exec pips.stp_EmployeeUserSyncUpdate " +
+                                                "  '" + strUsername + "', " +
+                                                "  '" + strEmployeeNo + "', " +
+                                                "  '" + strEmployeeName + "', " +
+                                                "  '" + strMailAddress + "', " +
+                                                "  '" + strOrganizationCode + "', " +
+                                                "  '" + strOrganizationName + "', " +
+                                                "  '" + strJobPositionCode + "', " +
+                                                "  '" + strJobPositionName + "', " +
+                                                "  '" + strLeader + "', " +
+                                                "  '" + strSM + "', " +
+                                                "  '" + strGM + "', " +
+                                                "  '" + strDir + "', " +
+                                                "  '" + ClsAuthSession.USERNAME + "', " +
+                                                "  @intOutStatus out, " +
+                                                "  @strOutMessage out; " +
+                                                "select pips.to_char(@intOutStatus,null)+'#'+@strOutMessage; " +
+                                                "end;");
+
+                string[] arrExec = strExec.Split('#');
+                long intStatus = Convert.ToInt64(arrExec[0].ToString());
+                string strMsg = arrExec[1].ToString();
+
+                if (intStatus == 0)
+                {
+                    int intLog = WsData.ExecuteNonQuery("begin " +
+                                                "exec pips.stp_APILogger " +
+                                                  "'SyncEmployee_HCM_EPROC', " +
+                                                  "'{\"employeeNo\":\"" + strEmployeeNo + "\"}', " +
+                                                  "'0', " +
+                                                  "'" + strMsg + "', " +
+                                                  "'" + ClsAuthSession.USERNAME + "'; " +
+                                                "end;");
+                }
+
+            }
+
+            txtUserName.Text = strFilter_username;
+            txtSearch.Text = strFilter_search;
+            txtOrganizationName.Text = strFilter_organization;
+
+            ViewData(1);
+
+            ClientScriptManager cs = Page.ClientScript;
+            Type cstype = this.GetType();
+            cs.RegisterStartupScript(cstype, null, "<script language='javascript'>alert('Sinkronisasi telah dilakukan. Silakan periksa kembali data');</script>");
+            return;
+        }
+        catch (Exception ex)
+        {
+
+            ClientScriptManager cs = Page.ClientScript;
+            Type cstype = this.GetType();
+            cs.RegisterStartupScript(cstype, null, "<script language='javascript'>alert('Sinkronisasi GAGAL dilakukan. Silakan periksa kembali Sumber Data');</script>");
+            return;
+
+        }
+
+
+    }
+}
